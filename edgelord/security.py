a46f5f590a56171891fc16073f9e334592347342
DylanEHolland <salinson1138@gmail.com>
2019-09-03 12:07:55 -0500

    implemented basic sma

--- edgelord/security.py
+++ edgelord/security.py
@@ -7,19 +7,34 @@ class security:
     # Statistics
     #
     def changes(self, days = None, price_type = 'close'):
+        """Daily changes in price"""
+
         return self.load_data(days, price_type).pct_change()
 
+    # 
+    #
     def growth(self, days = None, price_type = 'close'):
         """Return percent growth for the given period"""
+
         data = self.load_data(days, price_type)
         
         # new - old / old = percent growth
         return (data.iloc[-1] - data.iloc[0]) / data.iloc[0]
 
+    #
+    #
     def sharpe(self, days = None, price_type = 'close'):
         """Return the equities sharpe ratio"""
         
         return (self.growth(days, price_type) - self.risk_free_rate) / self.standard_deviation(days, price_type)
+
+    #  
+    #
+    def simple_moving_average(self, days = None, price_type = 'close'):
+        """Return the simple moving average"""
+
+        data = self.load_data(days, price_type).mean()
+        print(data)
 
     def standard_deviation(self, days = None, price_type = 'close'):
         return self.load_data(days, price_type).std()
@@ -32,12 +47,18 @@ class security:
         self.data = security_data.security_data().from_csv(file)
         return self
 
+    #
+    #
     def load_data(self, days = None, price_type = 'close'):
+        """Load data for the time period"""
+
         if days is None:
             days = len(self.data.frame().index)
         
         return self.data.frame()[price_type].tail(days)
 
+    #
+    #
     def risk_free(self, treasury_yield):
         """Set the risk free growth rate"""
 
